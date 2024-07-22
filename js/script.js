4d4213ef557ff459d447f6109f4fa9d1713f5078
TK <118142360+tak40@users.noreply.github.com>
2024-07-22 09:36:28 -0400

    feat: Implement dynamic background image fetching from Unsplash
    - Integrated Unsplash API to fetch and display a random background image
    - Included error handling to use a default background image and author in case of a failed fetch
    - Ensured the background image and author attribution only update if the window width is greater than 600px
    
    Details:
    - Used async function  to handle API requests to Unsplash
    - Updated HTML to include an element for displaying background image author
    
    Files modified:
    - script.js: Added JavaScript to fetch and display background images from Unsplash

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const quote = document.querySelector(".quote")
 const author = document.querySelector(".author")
+const body = document.querySelector("body")
+const backgroundAuthor = document.querySelector(".background-author")
 
 async function quoteOfTheDay() {
     const url =
@@ -30,3 +32,28 @@ async function quoteOfTheDay() {
 }
 
 quoteOfTheDay()
+
+async function fetchBackgroundImage() {
+    const url =
+        "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
+
+    try {
+        const response = await fetch(url)
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
+        const data = await response.json()
+        if (window.innerWidth > 600) {
+            body.style.backgroundImage = `url(${data.urls.regular})`
+            backgroundAuthor.textContent = `Image Courtesy of: ${data.user.name}`
+        }
+    } catch (error) {
+        // Use a default background image/author
+        if (window.innerWidth > 600) {
+            body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080)`
+            backgroundAuthor.textContent = `By: Dodi Achmad`
+        }
+    }
+}
+
+fetchBackgroundImage()
